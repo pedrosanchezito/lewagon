6e2df1a3441d1d1457c6f8ea47c44f3da43beeed
Sébastien Saunier <seb@saunier.me>
2018-11-26 15:12:04 +0100

    Adding a third test to Sudoku

--- 01-OOP/09-Optional-Sudoku/tests/test_sudoku.py
+++ 01-OOP/09-Optional-Sudoku/tests/test_sudoku.py
@@ -20,7 +20,26 @@ class SudokuSolvertest(unittest.TestCase):
         solver = SudokuSolver(grid)
         self.assertTrue(solver.is_valid())
 
+
     def test_invalid_grid(self):
+        grid = [
+            [8,8,4,  1,5,9,  2,2,6],
+            [5,2,9,  6,8,2,  8,4,1],
+            [6,1,2,  4,2,8,  8,5,9],
+
+            [9,2,8,  8,1,5,  4,6,2],
+            [2,5,8,  8,4,6,  1,9,2],
+            [4,6,1,  9,2,2,  5,8,8],
+
+            [8,8,6,  2,9,4,  2,1,5],
+            [2,4,2,  5,6,1,  9,8,8],
+            [1,9,5,  2,8,8,  6,2,4]
+        ]
+
+        solver = SudokuSolver(grid)
+        self.assertEqual(solver.is_valid(), False)
+
+    def test_invalid_grid2(self):
         grid = [
             [1,2,3, 4,5,6, 7,8,9],
             [2,3,1, 5,6,4, 8,9,7],
