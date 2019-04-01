459cea8168e9c6c658c6ddb74e15a1cc4189c724
pedrosanchezito <ps8560251@gmail.com>
2019-04-01 16:46:11 +0200

    finir le soduku

--- 01-OOP/09-Optional-Sudoku/sudoku.py
+++ 01-OOP/09-Optional-Sudoku/sudoku.py
@@ -4,6 +4,32 @@ class SudokuSolver:
     def __init__(self, grid):
         self.grid = grid
 
-    def is_valid(self):
-        # TODO: implement this!
-        pass
+    def is_valid_row(self):
+        columns = []
+        for row in self.grid:
+            if len(set(row)) != len(row):
+                return False
+
+            for col in range(9):
+                columns[col].append(row[col])
+
+        for column in columns:
+            if len(set(column)) != len(column):
+                return False
+
+
+        return True
+
+grid = [
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [1,2,3, 4,5,6, 7,8,9],
+    [2,3,1, 6,6,4, 8,9,7]
+]
+s = SudokuSolver(grid)
+print(s.is_valid_row())
