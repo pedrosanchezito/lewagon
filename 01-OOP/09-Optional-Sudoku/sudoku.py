d9454e6be88b27cf0ef499e7713f80a2d6af2110
pedrosanchezito <ps8560251@gmail.com>
2019-04-01 18:23:54 +0200

    exos a finir

--- 01-OOP/09-Optional-Sudoku/sudoku.py
+++ 01-OOP/09-Optional-Sudoku/sudoku.py
@@ -4,32 +4,31 @@ class SudokuSolver:
     def __init__(self, grid):
         self.grid = grid
 
-    def is_valid_row(self):
-        columns = []
-        for row in self.grid:
-            if len(set(row)) != len(row):
+    def is_valid(self):
+
+        for i,row in enumerate(self.grid):
+            col = [row[i] for row in self.grid]
+
+            if len(set(row)) != len(row) or len(set(col)) != len(col):
                 return False
 
-            for col in range(9):
-                columns[col].append(row[col])
 
-        for column in columns:
-            if len(set(column)) != len(column):
-                return False
 
 
         return True
 
 grid = [
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [1,2,3, 4,5,6, 7,8,9],
-    [2,3,1, 6,6,4, 8,9,7]
+    [8,8,4,  1,5,9,  2,2,6],
+    [5,2,9,  6,8,2,  8,4,1],
+    [6,1,2,  4,2,8,  8,5,9],
+
+    [9,2,8,  8,1,5,  4,6,2],
+    [2,5,8,  8,4,6,  1,9,2],
+    [4,6,1,  9,2,2,  5,8,8],
+
+    [8,8,6,  2,9,4,  2,1,5],
+    [2,4,2,  5,6,1,  9,8,8],
+    [1,9,5,  2,8,8,  6,2,4]
 ]
 s = SudokuSolver(grid)
-print(s.is_valid_row())
+print(s.is_valid())
